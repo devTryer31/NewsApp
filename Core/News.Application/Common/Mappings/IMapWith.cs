6089ba824b94727471dc4130c8b6edd5aef1d382
Валера Герасимов <valera.gs@bk.ru>
2022-05-08 21:43:56 +0300

    Mapping base done.

---
+++ Core/News.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace News.Application.Common.Mappings
+{
+    public interface IMapWith<T>
+    {
+        void CreateMapping(Profile profile) => profile.CreateMap(typeof(T), GetType());//TODO: check for error.
+    }
+}
