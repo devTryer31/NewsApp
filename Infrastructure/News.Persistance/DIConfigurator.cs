8dd3f8be29b251c81a92e2a951f6246a96d98677
Валера Герасимов <valera.gs@bk.ru>
2022-05-09 01:29:04 +0300

    Application and Persistance DI configurators added.

---
+++ Infrastructure/News.Persistance/DIConfigurator.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using News.Application.Interfaces;
+
+namespace News.Persistence
+{
+    public static class DIConfigurator
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            string connString = configuration.GetConnectionString("SQLite");
+            services.AddDbContext<INewsDbContext, NewsDbContext>(opt => opt.UseSqlite(connString));
+
+            return services;
+        }
+    }
+}
