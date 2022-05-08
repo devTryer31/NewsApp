6a2370f20daa671531f678b86f6e340720c33012
Валера Герасимов <valera.gs@bk.ru>
2022-05-08 18:44:13 +0300

    Add project files.

---
+++ Core/News.Domain/News.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace News.Domain
+{
+    public class News
+    {
+        public Guid NewsId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime LastModifiedDate { get; set; }
+
+        public Guid User { get; set; }
+    }
+}
